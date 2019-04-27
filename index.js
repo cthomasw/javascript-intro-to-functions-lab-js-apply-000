683ef7c8399f8c89f661bc22f7707110fdd931fe
mathematical-attribute-1144 <claire.wilbert@gmail.com>
2019-04-27 20:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,7 @@ if (string.toLowerCase() === string) {
 if (string.toUpperCase() === string) {
   return 'YES INDEED!'
 }
+if (string === 'I love you, Grandma.') {
+  return 'I love you, too.'
+}
 }
